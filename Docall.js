ca77a2e92544b6de3c5cb6cc7b481da018ecb51d
mfonmfon <mfonm579@gmail.com>
2024-08-18 08:35:38 +0100

    commit

--- Docall.js
+++ Docall.js
@@ -1,6 +1,41 @@
 
+
     const SIGN_UP = "http://localhost:8070/user/sign-up";
-    const  LOGIN = "http://localhost:8070/user/login";
-    const LOGOUT = "http://localhost:8070/user/logout";
+    // const  LOGIN = "http://localhost:8070/user/login";
+    // const LOGOUT = "http://localhost:8070/user/logout";
+
+
+    const form = document.getElementById("form")
+    // const url = SIGN_UP;
+    // console.log(form)
+
+    form.addEventListener('submit',async (event) => {
+        event.preventDefault()
+
+        const firstName = document.getElementById("firstName").value
+        const lastName = document.getElementById("lastName").value
+        const phoneNumber = document.getElementById("phoneNumber").value
+        const  email = document.getElementById("email").value
+
+        console.log(firstName)
+
+        const data = await fetch(SIGN_UP, {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json"
+            },
+            body: JSON.stringify({firstName:firstName, lastName:lastName, phoneNumber: phoneNumber, email: email})
+        })
+
+        if (data.ok) {
+            console.log('successfully Sign up')
+            // window.location.href=''
+        }else{
+           alert("Did not signup")
+        }
+    })
+
+
+
 
 
